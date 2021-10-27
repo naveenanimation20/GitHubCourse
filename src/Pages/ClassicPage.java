1d1ff40b1c13093301038274a04567cabeb36541
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-10-27 12:07:42 +0530

    Create ClassicPage.java

---
+++ src/Pages/ClassicPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+public class ClassicPage {
+
+	public void classic() {
+		int k = 100;
+		int k1 = 200;
+		int div = k1/k;
+		System.out.println(div);
+    System.out.println("Bye");
+
+	}
+
+}
