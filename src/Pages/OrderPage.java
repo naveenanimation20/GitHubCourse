92c78a7a418a7a0d47a2dcf153fdb136f8e13c82
kushanbhai <kushanchoudhury6@gmail.com>
2022-05-20 22:11:38 +0530

    order page details updated

--- src/Pages/OrderPage.java
+++ src/Pages/OrderPage.java
@@ -4,6 +4,6 @@ public class OrderPage {
 
 	
 	public static void doOrder() {
-		System.out.println("Order Page1");
+		System.out.println("Order Page2");
 	}
 }
--- src/tests/OrderPageTest.java
+++ src/tests/OrderPageTest.java
@@ -3,6 +3,6 @@ package tests;
 public class OrderPageTest {
 
 	public void orderPageTest() {
-		System.out.println(false);
+		System.out.println(true);
 	}
 }
